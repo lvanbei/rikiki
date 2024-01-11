6d0090268faca324f66ed5b26048cc75eae41d97
Léopold Van Beirs <lvanbeirs@deloitte.com>
2024-01-12 00:02:24 +0100

    delete wrong data

--- lib/screens/base/base_cubit.dart
+++ lib/screens/base/base_cubit.dart
@@ -13,15 +13,11 @@ class BaseCubit extends Cubit<BaseState> {
   void onWidgetDidInit() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final players = prefs.getString("players");
-    final List<PlayerModel> parsedPlayers = players != null
-        ? (jsonDecode(players) as List)
-            .map((e) => PlayerModel.fromJson(e))
-            .toList()
-        : [];
+    final List<PlayerModel> parsedPlayers =
+        players != null ? _parsePlayers(players, prefs) : [];
     final game = prefs.getString("game");
-    final GameModel parsedGame = game != null
-        ? (GameModel.fromJson(jsonDecode(game)))
-        : const GameModel();
+    final GameModel parsedGame =
+        game != null ? _parseGame(game, prefs) : const GameModel();
 
     emit(BaseLoadedState(
       prefs: prefs,
@@ -29,6 +25,26 @@ class BaseCubit extends Cubit<BaseState> {
       listOfPlayers: parsedPlayers,
       round: parsedGame.round,
     ));
+  }
+
+  List<PlayerModel> _parsePlayers(players, SharedPreferences prefs) {
+    try {
+      return (jsonDecode(players) as List)
+          .map((e) => PlayerModel.fromJson(e))
+          .toList();
+    } catch (e) {
+      prefs.setString('players', '');
+      return [];
+    }
+  }
+
+  GameModel _parseGame(game, SharedPreferences prefs) {
+    try {
+      return (GameModel.fromJson(jsonDecode(game)));
+    } catch (e) {
+      prefs.setString('game', '');
+      return const GameModel();
+    }
   }
 
   Future updatePlayers(List<PlayerModel> players) async {
