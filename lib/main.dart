427f0d82f77dd721ffdc499d887377408afb33ca
Léopold Van Beirs <lvanbeirs@deloitte.com>
2024-01-25 16:20:17 +0100

    test to remove tmp file

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,3 @@ void main() {
 // https://pub.dev/packages/pwa_install
 
 // https://progressier.com/pwa-icons-and-ios-splash-screen-generator
-
-//  https://pub.dev/packages/dart_pre_commit#installation
