45d49eedab7e40773176f0387d977d8939a233f0
Léopold Van Beirs <lvanbeirs@deloitte.com>
2024-02-06 00:06:40 +0100

    keep bottom symetric

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:rikiki/screens/screens.dart';
+import 'package:rikiki_for_real/screens/screens.dart';
 import 'package:universal_html/html.dart' as html;
 
 import '../../core/core.dart';
@@ -92,14 +92,7 @@ class BaseScreen extends StatelessWidget {
             ),
             body: SafeArea(
                 child: Padding(
-                    padding: EdgeInsets.only(
-                        top: 24,
-                        bottom: kIsWeb &&
-                                html.window
-                                    .matchMedia('(display-mode: standalone)')
-                                    .matches
-                            ? 60
-                            : 24),
+                    padding: const EdgeInsets.only(top: 24, bottom: 24),
                     child: child)),
           );
         }
