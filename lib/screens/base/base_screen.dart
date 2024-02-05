f622c2da153413d47445e90cd882437b0b22c8da
Léopold Van Beirs <lvanbeirs@deloitte.com>
2024-02-06 00:08:59 +0100

    keep bottom symetric

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:rikiki_for_real/screens/screens.dart';
-import 'package:universal_html/html.dart' as html;
+import 'package:rikiki/screens/screens.dart';
 
 import '../../core/core.dart';
 
