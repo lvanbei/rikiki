3ace250a7527d406db438a0ef0137f5bef8eadcd
Léopold Van Beirs <lvanbeirs@deloitte.com>
2024-02-07 14:24:43 +0100

    push google analytics

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
-
-
-// https://pub.dev/packages/pwa_install
-
-// https://progressier.com/pwa-icons-and-ios-splash-screen-generator
-
-// https://medium.com/@azharbinanwar/change-the-app-theme-using-flutter-bloc-hydrated-bloc-42d091482829
